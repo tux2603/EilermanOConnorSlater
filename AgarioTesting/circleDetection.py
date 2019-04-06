de28b3f2395bde45992268f9c24be2ea83cc0cf0
Ryan <*PnJNSuTEZ%5ZQi7WSdT%tL%Bvk1od6xm>
2019-04-06 12:18:46 -0400

    Auto death detection and quitting

--- AgarioTesting/circleDetection.py
+++ AgarioTesting/circleDetection.py
@@ -32,21 +32,21 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('Pictures/Screenshots/Screenshot-20190403112444-1270x663.png',0)
+img = cv2.imread(
+    'Pictures/Screenshots/Screenshot-20190403112444-1270x663.png', 0)
 
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-lower_red = np.array([30,150,50])
-upper_red = np.array([255,255,180])
+lower_red = np.array([30, 150, 50])
+upper_red = np.array([255, 255, 180])
 
 mask = cv2.inRange(hsv, lower_red, upper_red)
-res = cv2.bitwise_and(img,img, mask= mask)
+res = cv2.bitwise_and(img, img, mask=mask)
 
-cv2.imshow('Original',img)
-edges = cv2.Canny(img,100,200)
-cv2.imshow('Edges',edges)
-
+cv2.imshow('Original', img)
+edges = cv2.Canny(img, 100, 200)
+cv2.imshow('Edges', edges)
 
 
 # edges = cv2.Canny(img, 100, 200)
@@ -57,6 +57,3 @@ cv2.imshow('Edges',edges)
 # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
 # plt.show()
-
-
-
